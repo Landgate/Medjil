dccf3208a237c812af2e6607dabe14f84603f01c
nakhap <Khandu2000@gmail.com>
2022-08-31 09:21:54 +0800

    Deleting everything

--- baseline_calibration/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppBaselineCalibrationConfig(AppConfig):
-    name = 'baseline_calibration'
--- calibrationsites/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalibrationsitesConfig(AppConfig):
-    name = 'calibrationsites'
