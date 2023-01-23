aa02689c29b73e31fe09a06eb2fb4fca549b7d8e
KentWheeler <48744654+KentWheeler@users.noreply.github.com>
2023-01-23 11:56:35 +0800

    Increase Limit for fields on forms

--- instrument_calibrations/settings.py
+++ instrument_calibrations/settings.py
@@ -151,6 +151,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 #SECURITY SETTINGS
 #CSRF Protections
