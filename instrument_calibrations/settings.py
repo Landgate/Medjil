dcf2e9f97c313ca42ccfbf3833fa79746d0f229a
nakhap <Khandu2000@gmail.com>
2023-02-10 11:37:35 +0800

    Adding database options

--- instrument_calibrations/settings.py
+++ instrument_calibrations/settings.py
@@ -95,16 +95,25 @@ WSGI_APPLICATION = 'instrument_calibrations.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
-        # "USER": os.environ.get("SQL_USER", "user"),
-        # "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        # "HOST": os.environ.get("SQL_HOST", "localhost"),
-        # "PORT": os.environ.get("SQL_PORT", "5432"),
+if 'SQL_ENGINE' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': os.environ.get('SQL_ENGINE'),
+            'NAME': os.environ.get('SQL_DATABASE'),
+            'USER': os.environ.get('SQL_USER'),
+            'PASSWORD': os.environ.get('SQL_PASSWORD'),
+            'HOST': os.environ.get('SQL_HOST'),
+            'PORT': os.environ.get('SQL_PORT'),
+
+        }
+    } 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+        }
     }
-}
 
 #DJANG MESSAGE
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
