543d645d5ed472864d7d8a3041464999c9269e16
KentWheeler <48744654+KentWheeler@users.noreply.github.com>
2024-07-02 17:16:54 +0800

    content_type = 'image/svg+xml'
    The content type needed to be be explicitly stated for the svg to save correctly in S# bucket
    
    Signed-off-by: KentWheeler <48744654+KentWheeler@users.noreply.github.com>

--- staffcalibration/views.py
+++ staffcalibration/views.py
@@ -226,7 +226,7 @@ def save_render_figure(staff_interval, tmp_sf1, calib_id):
         ax.yaxis.set_major_formatter(FormatStrFormatter('% 1.2f'))
         fig.savefig(buffer, bbox_inches='tight', format='svg', dpi=300)
         buffer.seek(0)
-        image_file = SimpleUploadedFile('StaffError', buffer.read())
+        image_file = SimpleUploadedFile('StaffError', buffer.read(), content_type = 'image/svg+xml')
         # Save the figure to model
         calib_id.calibration_error = image_file
         calib_id.save()  
